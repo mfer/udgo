7a2a90f6168581dd799f4333c7a194c1d6a2dd6a
sudolshw@gmail.com <sudolshw@gmail.com>
2014-05-21 23:22:47 -0300

    automatic contiki adaptation to accepted UDGO

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ udgo
 ====
 
 unit disk graph obstructed app fo cooja
+
+#git clone https://github.com/mfer/udgo.git
+#cd udgo
+#./contiki.sh
---
+++ contiki.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#after --> git clone https://github.com/mfer/udgo.git
+#cd udgo
+#./contiki.sh
+cd ../
+git clone https://github.com/contiki-os/contiki.git
+cd contiki
+git submodule update --init
+
+\cp ../udgo/contiki/tools/cooja/build.xml tools/cooja/
+\cp ../udgo/contiki/tools/cooja/config/cooja.html tools/cooja/config/
+\cp ../udgo/contiki/tools/cooja/config/cooja_applet.config tools/cooja/config/
+\cp ../udgo/contiki/tools/cooja/config/external_tools.config tools/cooja/config/
+
+mkdir tools/cooja/apps/udgo
+cd tools/cooja/apps/udgo
+ln -s ../../../../../udgo/contiki/tools/cooja/apps/udgo/* .
+
+
+
+cd ../../../../tools/cooja/
+ant run_bigmem
