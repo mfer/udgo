1b835a1d16c89b1b8a86225b4f9dff196b7d7d37
mfer <sudolshw@gmail.com>
2014-05-31 20:37:17 -0300

    changing to receive parameters from command line

--- starts.sh
+++ starts.sh
@@ -1,8 +1,14 @@
-cd
-cd udgo/sampleGen
-javac Position.java && java Position 8 6 10
-javac CSCgen.java && java CSCgen 8 6 10
-cp 8-6-10.csc ../../contiki/tools/cooja/
-cp 8-6-10.sensor ../sampleAnalize/
-cd ../../contiki/tools/cooja
-ant run_nogui -Dargs=/home/mfer/contiki/tools/cooja/8-6-10.csc &
+#parameter 1 g
+#parameter 2 mu
+#parameter 3 eps
+
+echo cd
+echo cd udgo/sampleGen
+echo javac Position.java
+echo java Position $1 $2 $3
+echo javac CSCgen.java
+echo java CSCgen $1 $2 $3
+echo cp $1-$2-$3.csc ../../contiki/tools/cooja/
+echo cp $1-$2-$3.sensor ../sampleAnalize/
+echo cd ../../contiki/tools/cooja
+echo ant run_nogui -Dargs=/home/mfer/contiki/tools/cooja/$1-$2-$3.csc &
