299c9e313dc7f70bfa58dc1740b40ca7f8d7b110
mfer <sudolshw@gmail.com>
2014-08-30 18:34:03 -0300

    java classpath added

--- scripts/ends.sh
+++ scripts/ends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#parameter 1 dir_name
+#parameter 1 dirname
 #parameter 2 g
 #parameter 3 mu
 #parameter 4 eps
@@ -10,12 +10,12 @@ if [ $# -eq 4 ]
     cd ../src/sample/analize
     tail -n +$2 $2-$3-$4.testlog | head -n -3 > $2-$3-$4.testlog.new
     mv $2-$3-$4.testlog.new $2-$3-$4.testlog
-    javac CTRgen.java
+    javac -cp . CTRgen.java
     java CTRgen $2 $3 $4
     mv $2-$3-$4.ctr CTR/
     cd CTR
-    javac CTR.java
+    javac -cp . CTR.java
     java CTR $2-$3-$4.ctr >> $2-$3-$4.ctrs
   else
-    echo "No arguments supplied: usage: ./setup-contiki.sh dir_name g mu eps"
+    echo "No arguments supplied: usage: ./ends.sh dirname g mu eps"
 fi
