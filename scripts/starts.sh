5185df906ad708ba05a6d71ee7f6c69fb0799a4b
mfer <sudolshw@gmail.com>
2014-10-22 08:52:31 -0200

    changes

--- scripts/starts.sh
+++ scripts/starts.sh
@@ -16,8 +16,8 @@ if [ $# -eq 3 ]
     javac CSCgen.java
     java -cp . CSCgen $g $mu $eps
     cp $g-$mu-$eps.csc  ../../../build/$dirname/contiki/tools/cooja/
-    cp ../../contiki/tools/cooja/java/org/contikios/cooja/plugins/VariableWatcher.java  ../../../build/$dirname/contiki/tools/cooja/plugin/
-    cp $g-$mu-$eps.sensor ../analize/
+    cp ../../contiki/tools/cooja/java/org/contikios/cooja/plugins/VariableWatcher.java  ../../../build/$dirname/contiki/tools/cooja/plugin
+/    cp $g-$mu-$eps.sensor ../analize/
     cd ../../../build/$dirname/contiki/tools/cooja/
     var=$(pwd)
     ant run_nogui -Dargs=$var/$g-$mu-$eps.csc #> /dev/null 2>&1 &
