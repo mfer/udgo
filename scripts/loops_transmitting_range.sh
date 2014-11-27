349ecf00763d5738ade0063fe42252a15b95388a
mfer <sudolshw@gmail.com>
2014-11-27 08:36:43 -0200

    setting transmission ranges for multiple mu_s

--- scripts/loops_transmitting_range.sh
+++ scripts/loops_transmitting_range.sh
@@ -19,11 +19,11 @@ do
 
     filename="../build/"$dirname"/contiki/tools/cooja/apps/udgo/java/org/contikios/udgo/UDGO.java"
     echo $filename
-    find $filename -type f -print0 | xargs -0 sed -i 's/setTxRange($old)/setTxRange($new)/g'
+    find $filename -type f -print0 | xargs -0 sed -i 's/setTxRange('$old')/setTxRange('$new')/g'
 
     filename="../build/"$dirname"/contiki/tools/cooja/apps/udgo/java/org/contikios/udgo/ChannelModel.java"
     echo $filename
-    find $filename -type f -print0 | xargs -0 sed -i 's/source.distance(dest) < $old/source.distance(dest) < $new/g'    
+    find $filename -type f -print0 | xargs -0 sed -i 's/source.distance(dest) < '$old'/source.distance(dest) < '$new'/g'    
   done
 done
 
