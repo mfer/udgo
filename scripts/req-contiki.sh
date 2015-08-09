f019da850cc9128d523a4e3b552830dca0bf68ab
Manassés Ferreira Neto <sudolshw@gmail.com>
2015-08-09 10:48:17 -0300

    Update req-contiki.sh

--- scripts/req-contiki.sh
+++ scripts/req-contiki.sh
@@ -1,1 +1,1 @@
-sudo apt-get install build-essential binutils-msp430 gcc-msp430 msp430-libc binutils-avr gcc-avr gdb-avr avr-libc avrdude openjdk-7-jdk openjdk-7-jre ant libncurses5-dev git
+sudo apt-get update && sudo apt-get install build-essential binutils-msp430 gcc-msp430 msp430-libc binutils-avr gcc-avr gdb-avr avr-libc avrdude openjdk-7-jdk openjdk-7-jre ant libncurses5-dev git
